df77b64d4f334e342e22856add41e235d951ac5c
jimcppdp <jimcppdp@gmail.com>
2020-11-21 13:10:07 +0800

    fix window no show

--- 01_hello_SDL/01_hello_SDL.cpp
+++ 01_hello_SDL/01_hello_SDL.cpp
@@ -11,46 +11,62 @@ const int SCREEN_HEIGHT = 480;
 
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+  //The window we'll be rendering to
+  SDL_Window* window = NULL;
+  
+  //The surface contained by the window
+  SDL_Surface* screenSurface = NULL;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
+  //Initialize SDL
+  if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+  {
+    printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+  }
+  else
+  {
+    //Create window
+    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( window == NULL )
+    {
+      printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+      //Get window surface
+      screenSurface = SDL_GetWindowSurface( window );
 
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+      //Fill the surface white
+      SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+      
+      //Update the surface
+      SDL_UpdateWindowSurface( window );
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
-		}
-	}
+      SDL_Event e;
+      bool quit = false;
+      while (!quit){
+          while (SDL_PollEvent(&e)){
+              if (e.type == SDL_QUIT){
+                  quit = true;
+              }
+              if (e.type == SDL_KEYDOWN){
+                  quit = true;
+              }
+              if (e.type == SDL_MOUSEBUTTONDOWN){
+                  quit = true;
+              }
+          }
+      }
 
-	//Destroy window
-	SDL_DestroyWindow( window );
+      //Wait two seconds
+      //SDL_Delay( 2000 );
+    }
+  }
 
-	//Quit SDL subsystems
-	SDL_Quit();
+  //Destroy window
+  SDL_DestroyWindow( window );
 
-	return 0;
+  //Quit SDL subsystems
+  SDL_Quit();
+
+  return 0;
 }
